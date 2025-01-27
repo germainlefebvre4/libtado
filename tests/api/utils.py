4dc421ed9877cbecd951debbf2912fd47889e961
Germain LEFEBVRE <germain.lefebvre4@gmail.com>
2025-01-27 15:34:17 +0100

    chore: Generate JSON Schemas from API

--- tests/api/utils.py
+++ tests/api/utils.py
@@ -11,10 +11,6 @@ tado = Tado(TADO_USERNAME, TADO_PASSWORD, TADO_CLIENT_SECRET)
 
 
 class TestApi:
-    def get_zones():
-        response = tado.get_zones()
-        return response
-
     def get_capabilities():
         ZONE_ID = tado.get_zones()[0]["id"]
         response = tado.get_capabilities(ZONE_ID)
@@ -41,10 +37,6 @@ class TestApi:
         response = tado.get_home_state()
         return response
 
-    def get_installations():
-        response = tado.get_home()
-        return response
-
     def get_invitations():
         response = tado.get_invitations()
         return response
@@ -57,14 +49,43 @@ class TestApi:
         response = tado.get_mobile_devices()
         return response
 
+    def get_schedule_timetables():
+        ZONE_ID = tado.get_zones()[0]["id"]
+        response = tado.get_schedule_timetables(ZONE_ID)
+        return response
+
     def get_schedule():
         ZONE_ID = tado.get_zones()[0]["id"]
         response = tado.get_schedule(ZONE_ID)
         return response
 
+    def get_schedule_blocks():
+        ZONE_ID = tado.get_zones()[0]["id"]
+        SCHEDULE_ID = tado.get_schedule(ZONE_ID)["id"]
+        response = tado.get_schedule_blocks(ZONE_ID, SCHEDULE_ID)
+        return response
+
+    def get_schedule_block_by_day_type():
+        ZONE_ID = tado.get_zones()[1]["id"]
+        # SCHEDULE = tado.get_schedule(ZONE_ID)
+        SCHEDULE_ID = tado.get_schedule(ZONE_ID)["id"]
+        SCHEDULE_BLOCK_DAY_TYPE = tado.get_schedule_blocks(ZONE_ID, SCHEDULE_ID)[0]["dayType"]
+        response = tado.get_schedule_block_by_day_type(ZONE_ID, SCHEDULE_ID, SCHEDULE_BLOCK_DAY_TYPE)
+        return response
+
     def get_state():
         ZONE_ID = tado.get_zones()[0]["id"]
         response = tado.get_state(ZONE_ID)
+        return response
+
+    def get_measuring_device():
+        ZONE_ID = tado.get_zones()[0]["id"]
+        response = tado.get_measuring_device(ZONE_ID)
+        return response
+
+    def get_default_overlay():
+        ZONE_ID = tado.get_zones()[0]["id"]
+        response = tado.get_default_overlay(ZONE_ID)
         return response
 
     def get_users():
@@ -75,10 +96,31 @@ class TestApi:
         response = tado.get_weather()
         return response
 
+    def get_zones():
+        response = tado.get_zones()
+        return response
+
+    def get_away_configuration():
+        ZONE_ID = tado.get_zones()[0]["id"]
+        response = tado.get_away_configuration(ZONE_ID)
+        return response
+
     def get_report():
         ZONE_ID = tado.get_zones()[0]["id"]
         yesterday  = str(date.today() + relativedelta(days=-1))
         response = tado.get_report(ZONE_ID, yesterday)
+        return response
+
+    def get_heating_circuits():
+        response = tado.get_heating_circuits()
+        return response
+
+    def get_incidents():
+        response = tado.get_incidents()
+        return response
+
+    def get_installations():
+        response = tado.get_home()
         return response
 
     def get_air_comfort():
@@ -91,33 +133,41 @@ class TestApi:
         response = tado.get_air_comfort_geoloc(GEO_LATITUDE, GEO_LONGITUDE)
         return response
 
+    def get_heating_system():
+        response = tado.get_heating_system()
+        return response
+
+    def get_running_times():
+        FROM_DATE = str(date.today() + relativedelta(days=-1))
+        response = tado.get_running_times(FROM_DATE)
+        return response
+
+    def get_zone_states():
+        response = tado.get_zone_states()
+        return response
+
+    # def get_energy_consumption():
+    #     response = tado.get_energy_consumption()
+    #     return response
+
+    # def get_energy_savings():
+    #     MONTH = date.today().strftime("%Y-%m")
+    #     COUNTRY = "FRA"
+    #     response = tado.get_energy_savings(MONTH, COUNTRY)
+    #     return response
+
     def set_cost_simulation():
         country = "FRA"
         payload = {
-            "temperatureDeltaPerZone": [
-                {
-                    "zone": 1,
-                    "setTemperatureDelta": -1
-                },
-                {
-                    "zone": 1,
-                    "setTemperatureDelta": -1
-                },
-                {
-                    "zone": 1,
-                    "setTemperatureDelta": -1
-                },
-                {
-                    "zone": 1,
-                    "setTemperatureDelta": -1
-                }
-            ]
+            "temperatureDeltaPerZone": [{
+                "zone": 6,
+                "setTemperatureDelta": -1,
+            }]
         }
         response = tado.set_cost_simulation(country=country, ngsw_bypass=True, payload=payload)
         return response
 
     def get_consumption_overview():
-        # Get current datetime and convert to "YYYY-MM" format
         monthYear = date.today().strftime("%Y-%m") # 2023-09
         country = "FRA"
         response = tado.get_consumption_overview(monthYear=monthYear, country=country)
@@ -136,21 +186,3 @@ class TestApi:
     #     country = "FRA"
     #     response = tado.get_energy_insights(start_date=start_date, end_date=end_date, country=country)
     #     return response
-
-    def get_consumption_overview():
-        # Get current datetime and convert to "YYYY-MM" format
-        monthYear = date.today().strftime("%Y-%m")
-        country = "FRA"
-        response = tado.get_consumption_overview(monthYear=monthYear, country=country)
-        return response
-
-    def set_cost_simulation():
-        country = "FRA"
-        payload = {
-            "temperatureDeltaPerZone": [{
-                "zone": 6,
-                "setTemperatureDelta": -1,
-            }]
-        }
-        response = tado.set_cost_simulation(country=country, ngsw_bypass=True, payload=payload)
-        return response
