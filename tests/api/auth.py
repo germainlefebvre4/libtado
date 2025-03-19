86dc07c44f7d4091056c9c0d3dc107a1db9cca4e
Germain LEFEBVRE <germain.lefebvre4@gmail.com>
2025-03-20 00:47:50 +0100

    docs: Update tado generated jsonschemas

--- tests/api/auth.py
+++ tests/api/auth.py
@@ -1,8 +1,8 @@
 import os
 from libtado.api import Tado
 
-TADO_REFRESH_TOKEN = os.environ["TADO_REFRESH_TOKEN"]
-TADO_CREDENTIALS_FILE = os.environ["TADO_CREDENTIALS_FILE"]
+TADO_REFRESH_TOKEN = os.environ.get("TADO_REFRESH_TOKEN", "")
+TADO_CREDENTIALS_FILE = os.environ.get("TADO_CREDENTIALS_FILE")
 tado = Tado(TADO_REFRESH_TOKEN, TADO_CREDENTIALS_FILE)
 status = tado.get_device_activation_status()
 if status == "PENDING":
